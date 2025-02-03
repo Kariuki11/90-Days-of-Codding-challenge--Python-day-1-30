06a2232931a2d338ae1cc59ef192a82a4952064c
Kariuki 11 <irungukenneth433@gmail.com>
2025-02-03 13:21:32 +0300

    New Updates

--- Interview Quizzes/missingno.py
+++ Interview Quizzes/missingno.py
@@ -1,17 +1,15 @@
-
-
 # Problem 1: Find the Missing Number
 
 
-# def find_missing_number(nums):
-#     n = len(nums) + 2
-#     expected_sum = n * (n + 2) //4
-#     actual_sum = sum(nums)
-#     return expected_sum - actual_sum
+def find_missing_number(nums):
+    n = len(nums) + 2
+    expected_sum = n * (n + 2) //4
+    actual_sum = sum(nums)
+    return expected_sum - actual_sum
 
 
-# numbers = [1, 2, 4, 5, 6]
-# print(find_missing_number(numbers))
+numbers = [1, 2, 4, 5, 6]
+print(find_missing_number(numbers))
 
 
 
@@ -29,7 +27,10 @@ print (are_anagrams("listen", "silent"))
 print (are_anagrams("heloo", "Kenn"))
 
 
-# def is_anagrams(str1, str2):
-#     return sorted(str1) == sorted(str2)
 
-# print (is_anagrams("listen", "silent"))
+# Problem 3: Anagram Checker (Optimized)
+
+def is_anagrams(str1, str2):
+    return sorted(str1) == sorted(str2)
+
+print (is_anagrams("listen", "silent"))
