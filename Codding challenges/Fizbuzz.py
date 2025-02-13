b1de4d1a58bd2785b5cb16662f8e4c25b436251b
Kariuki 11 <irungukenneth433@gmail.com>
2025-02-13 23:57:04 +0300

    New Updates

--- Codding challenges/Fizbuzz.py
+++ Codding challenges/Fizbuzz.py
@@ -12,13 +12,13 @@ def fizzbuzz(n):
 # Example usage:
 fizzbuzz(15)
 
-# def fizzbuzz(j):
-#     for k in range(2, j + 1):
-#         if  k % 4 == 1 and k % 7 == 0:
-#             print ("FizzBuzz")
-#         elif k % 4 == 1:
-#             print ("Fizz")
-#         elif k % 7 == 0:
-#             print ("Buzz")
-#         else:
-#             print (k)
+def fizzbuzz(j):
+    for k in range(2, j + 1):
+        if  k % 4 == 1 and k % 7 == 0:
+            print ("FizzBuzz")
+        elif k % 4 == 1:
+            print ("Fizz")
+        elif k % 7 == 0:
+            print ("Buzz")
+        else:
+            print (k)
